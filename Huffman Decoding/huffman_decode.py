946f22f46987bbabe1aae5c230ec75ed92bc4cd9
Isaac <48895941+IsaacFigNewton@users.noreply.github.com>
2021-11-25 12:57:26 -0800

    Finished main huffman BST composing pseudocode

--- Huffman Decoding/huffman_decode.py
+++ Huffman Decoding/huffman_decode.py
@@ -16,15 +16,40 @@ def huffman_decode(encoded_message):
 
     #make a new list of tuples with the first index of each representing the ASCII code
     #and the second representing the frequency
-    stuffToPutInTree = []
+    stuffToPutInPQ = []
     #go through all the characters and their frequencies in steps of 2
     for i in range(0, charStuff, 2):
         #even indices are ASCII codes and odd are their frequencies
-        stuffToPutInTree[i/2] = (charStuff[i], charStuff[i + 1])
+        stuffToPutInPQ[i/2] = (charStuff[i], charStuff[i + 1])
+
 
 
     #You should build the Huffman tree from the header to get the encoding set (the code words
     #    representing the characters)
+
+    #create a priority queue with the ASCII characters and their frequencies
+    charFreqPQ = binary_heap.BinaryHeap()
+    for tuple in stuffToPutInPQ:
+        #add stuff to the PQ with its respetive key and value
+        charFreqPQ.enqueue(tuple[0], tuple[1])
+
+    #iterate until there's only there's only 1 node left in the priority queue, aka, if the BST has finished being created
+    while (priority queue size >= 2 nodes):
+        #remove the 2 least frequent characters from the priority queue
+        char1 = charFreqPQ.dequeue
+        char2 = charFreqPQ.dequeue
+        #create a node with a value equal to the sum of the frequencies of the 2 least frequent characters and make it the root of a new BST of the 3 nodes
+
+        #insert the new root node/BST with the combined frequency value into the priority queue
+
+
+
+    #decode/compose the message
+
+    decodedString = ""
+    #navigate to the respective character/leaf in the BST (0 = left, 1 = right)
+    #add the node's character to the decodedString
+    #move to the next binary character
 
 
     #You should check the given encoded message to detect errors in the encoding.
